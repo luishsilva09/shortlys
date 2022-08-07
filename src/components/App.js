a31b8eac430ac8d78723610d4aeeaaca7e15af33
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-07 20:47:26 -0300

    criando rotas e componentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.js";
+import SignIn from "./SignIn.js";
+import SignUp from "./SignUp.js"
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />}/>
+      <Route path="/signin"element={<SignIn />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
