974e622271927de4d2e2810c71a6ebe3c80521ae
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-08 13:03:53 -0300

    buscar lista do ranking

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,7 +1,32 @@
 import styled from "styled-components";
-import Trophy from "../assets/trophy.svg"
+import Trophy from "../assets/trophy.svg";
+import axios from 'axios'
+import React from "react";
+import {ThreeDots} from 'react-loader-spinner'
+
+function List({element,index}){
+    return (
+        <>
+          <p>{index+1}. {element.name} - {element.linksCount} links - {element.visitCount} visualizações</p>
+        </>
+    )
+
+}
 
 export default function Ranking(){
+    const [rankingData,setRankingData] = React.useState()
+    const [load,setLoad] =React.useState(true)
+
+
+    React.useEffect(()=> {
+        const promise = axios.get(' https://projeto16-back-shortly.herokuapp.com/ranking')
+        promise.then((req,res)=> {
+        setRankingData(req.data)
+        setLoad(false)
+    })
+    },[])
+    
+    
     return(
     <Container>
         <Title>
@@ -9,7 +34,7 @@ export default function Ranking(){
             <h1>Ranking</h1>
         </Title>
         <Table>
-
+            {load? <ThreeDots ></ThreeDots>:rankingData.map( ( e,index ) => <List element={e} key={index} index={index}/>)}
         </Table>
     </Container>)
 }
@@ -27,6 +52,13 @@ const Table = styled.div`
     border: 1px solid rgba(120, 177, 89, 0.25);
     box-shadow: 0px 4px 24px rgba(120, 177, 89, 0.12);
     border-radius: 24px 24px 0px 0px;
+    padding:20px 40px 20px 40px;
+    p{
+        font-size:22px;
+        font-weight: 400;
+        line-height: 28px;
+        margin-bottom: 13px;
+    }
 `
 const Title = styled.div`
     display: flex;
